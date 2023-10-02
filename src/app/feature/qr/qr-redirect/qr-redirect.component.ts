fbb0fa2aa2ae00611c160923cea36abf779e7756
dvillcar <diego.villa@devzeros.com>
2023-10-02 03:35:16 -0500

    cambios terminados

--- src/app/feature/qr/qr-redirect/qr-redirect.component.ts
+++ src/app/feature/qr/qr-redirect/qr-redirect.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, OnInit, Output, Renderer2} from '@angular/core';
 import {QrRedirectService} from "./shared/service/qr-redirect.service";
 import {QrRedirectInfo} from "./shared/interfaces/qr-redirect-info";
 import {TranslationService} from "../../store/store/shared/service/translate.service";
@@ -12,7 +12,7 @@ import {data} from "./shared/data-traslate-mock";
   providers: [TranslationService]
 })
 export class QrRedirectComponent implements OnInit{
-  constructor(private translationService: TranslationService)  {}
+  constructor(private translationService: TranslationService, private el: ElementRef, private renderer: Renderer2)  {}
   flags: Array<any> = [{
     language: 'spanish',
     code: 'es',
@@ -43,5 +43,21 @@ export class QrRedirectComponent implements OnInit{
 
   getCurrentLanguage() {
     return this.flags[this.currentLanguage].image
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const cards = this.el.nativeElement.querySelectorAll('.card');
+    const windowHeight = window.innerHeight;
+
+    cards.forEach((card: HTMLElement) => {
+      const rect = card.getBoundingClientRect();
+
+      // Comprobar si la parte superior de la tarjeta está en el viewport
+      if (rect.top >= 0 && rect.top <= windowHeight) {
+        // Agregar la clase 'visible' para activar la animación
+        this.renderer.addClass(card, 'visible');
+      }
+    });
   }
 }
